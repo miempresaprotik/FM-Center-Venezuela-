1a7dfbc517b0a5e02f89d7d144b782a2afd76d3b
miempresaprotik <miempresaprotik@gmail.com>
2026-04-07 11:54:41 -0400

    Modify fetch listener to handle specific requests
    Updated fetch event listener to only intercept audio files and GitHub requests.

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
 // Archivo: sw.js
 const CACHE_NAME = 'fm-center-v1';
 
+// Instalación rápida
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// El secreto está aquí: Interceptar la petición de música
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    // Solo interceptamos archivos de audio o peticiones de GitHub
+    if (event.request.url.includes('.mp3') || event.request.url.includes('raw.githubusercontent')) {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                // Si está en la "Lista Disponible" (caché), lo devuelve instantáneamente
+                // Si no está, lo busca en internet
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
